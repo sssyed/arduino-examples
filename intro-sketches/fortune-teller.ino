fd35f631820d56b34ff4635da2ba930ba8464d38
Syed Salahuddin <syedamin@cs.nyu.edu>
2015-05-18 18:16:14 -0400

    retry game without reset.

--- intro-sketches/fortune-teller.ino
+++ intro-sketches/fortune-teller.ino
@@ -1,86 +1,85 @@
-  // Made by Syed Salahuddin
-  // License: CC-BY-SA 3.0
+    // Made by Syed Salahuddin
+    // License: CC-BY-SA 3.0
 
-  /*
-    Fortune teller
+    /*
+      Fortune teller
 
-   The circuit:
+     The circuit:
 
-   */
+     */
 
-  // set pin numbers to represent our bits:
-  const int buttonPin = A0;     // the number of the pushbutton pin
-  const int servoPin = 5;    // the number of the Optional Buzzer pin
-  const int i = 0;
+    // set pin numbers to represent our bits:
+    const int buttonPin = A0;     // the number of the pushbutton pin
+    const int servoPin = 5;    // the number of the Optional Buzzer pin
+    const int i = 0;
 
-  int buttonState = 0;     // data for storing if our button has been pressed.
-  int randomNumber = 0;		//  data for storing a "random" number.
-  bool getRandomNumber = false;
-  bool servo = false;
-  bool changeDirection = false;
+    int buttonState = 0;     // data for storing if our button has been pressed.
+    int randomNumber = 0;		//  data for storing a "random" number.
+    bool getRandomNumber = false;
 
 
-  void setup() {
-    // initialize the Servo as an output:
-    pinMode(servoPin, OUTPUT);
+    void setup() {
+      // initialize the Servo as an output:
+      pinMode(servoPin, OUTPUT);
 
-    // initialize the pushbutton pin as an input:
-    pinMode(buttonPin, INPUT);
-    // Get a random number
+      // initialize the pushbutton pin as an input:
+      pinMode(buttonPin, INPUT);
+      // Get a random number
 
-  }
-
-  void loop(){
-    digitalWrite(5, 0);
-
-
-    if(getRandomNumber == false){
-      //attractMode();
     }
 
-    // read the state of the pushbutton value:
-    buttonState = digitalRead(buttonPin);
-    if(getRandomNumber == false){
-    	randomNumber = random(3);
-    }
+    void loop(){
+      //analogWrite(5, 10);
 
-    if (buttonState == HIGH) {
-      Serial.print(randomNumber);
-      getRandomNumber = true;
 
-      if (randomNumber == 1){
-        moveServo(1);  // moveServo to TRUE state
-      } else if(randomNumber == 2){
-        moveServo(2); // moveServo to MAYBE state
-      } else {
-        moveServo(0); // movesServo to FALSE state
+      if(getRandomNumber == false){
+        attractMode();
+      }
+
+      // read the state of the pushbutton value:
+      buttonState = digitalRead(buttonPin);
+      if(getRandomNumber == false){
+      	randomNumber = random(3);
+        delay(100);
+      }
+
+      if (buttonState == HIGH) {
+        Serial.print(randomNumber);
+        getRandomNumber = true;
+
+        if (randomNumber == 1){
+          moveServo(1);  // moveServo to TRUE state
+        } else if(randomNumber == 2){
+          moveServo(2); // moveServo to MAYBE state
+        } else {
+          moveServo(0); // movesServo to FALSE state
+        }
       }
     }
-  }
 
-  void moveServo(int fortune){
+    void moveServo(int fortune){
 
-    if(fortune == 1){
-      digitalWrite(5, 0);
+      if(fortune == 1){
+        analogWrite(5, 0);
+        delay(2000);
+      } else if(fortune == 2) {
+        //turn thumb to down position
+        analogWrite(5, 125);
+        delay(2000);
+        } else {
+        //turn thumb sides as "maybe"
+        analogWrite(5, 245);
+        delay(2000);
+      }
+    }
+
+    void attractMode(){
+      analogWrite(5, 0);
       delay(500);
-    } else if(fortune == 2) {
-      //turn thumb to down position
-      digitalWrite(5, 125);
+
+      analogWrite(5, 128);
       delay(500);
-      } else {
-      //turn thumb sides as "maybe"
-      digitalWrite(5, 255);
+
+      analogWrite(5, 250);
       delay(500);
     }
-  }
-
-  void attractMode(){
-    digitalWrite(5, 0);
-    delay(500);
-
-    digitalWrite(5, 125);
-    delay(500);
-
-    digitalWrite(5, 255);
-    delay(500);
-  }
