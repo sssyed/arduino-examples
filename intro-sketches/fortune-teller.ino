e8f549f72a871c18bd913794b3db22738931a3da
Syed Salahuddin <syedamin@cs.nyu.edu>
2015-05-18 10:32:51 -0400

    updates to fortune teller fixes servo motions

--- intro-sketches/fortune-teller.ino
+++ intro-sketches/fortune-teller.ino
@@ -1,98 +1,86 @@
-//  _ ___ _______     ___ ___ ___  ___ _   _ ___ _____ ___ 
-// / |_  )__ /   \   / __|_ _| _ \/ __| | | |_ _|_   _/ __| 
-// | |/ / |_ \ |) | | (__ | ||   / (__| |_| || |  | | \__ \ 
-// |_/___|___/___/   \___|___|_|_\\___|\___/|___| |_| |___/ 
-// 
-// buttons
-// 
+
 // Made by Syed Salahuddin
 // License: CC-BY-SA 3.0
-// Downloaded from: http://123d.circuits.io/circuits/779592-buttons
 
 /*
-  Button
- 
- Turns on and off a light emitting diode(LED) connected to digital  
- pin 13, when pressing a pushbutton attached to pin 2. 
- 
- 
+  Fortune teller
+
  The circuit:
- * LED attached from pin 13 to ground 
- * pushbutton attached to pin 2 from +5V
- * 10K resistor attached to pin 2 from ground
- 
- * Note: on most Arduinos there is already an LED on the board
- attached to pin 13.
- 
- 
- created 2005
- by DojoDave <http://www.0j0.org>
- modified 30 Aug 2011
- by Tom Igoe
- 
- This example code is in the public domain.
- 
- http://www.arduino.cc/en/Tutorial/Button
+
  */
 
-// constants won't change. They're used here to 
-// set pin numbers:
-const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
+// set pin numbers to represent our bits:
+const int buttonPin = A0;     // the number of the pushbutton pin
+const int servoPin = 5;    // the number of the Optional Buzzer pin
+const int i = 0;
 
-// variables will change:
-int buttonState = 0;         // variable for reading the pushbutton status
-int randomNumber = 0;		// generate random number that's between 0 - 2
+// variables to remember some important values
+int buttonState = 0;     // data for storing if our button has been pressed.
+int randomNumber = 0;		//  data for storing a "random" number.
 bool getRandomNumber = false;
-bool blinkOn = false;
+bool servo = false;
+bool changeDirection = false;
 
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);      
+  // initialize the Servo as an output:
+  pinMode(servoPin, OUTPUT);
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);     
+  pinMode(buttonPin, INPUT);
   // Get a random number
 
 }
 
 void loop(){
+
+  if(getRandomNumber == false){
+    //attractMode();
+  }
+
   // read the state of the pushbutton value:
   buttonState = digitalRead(buttonPin);
   if(getRandomNumber == false){
   	randomNumber = random(3);
-  } 
- 
-  // check if the pushbutton is pressed.
-  // if it is, the buttonState is HIGH:
-  if (buttonState == HIGH) { 
-    Serial.print(randomNumber);
-    delay( 1000 );
-    getRandomNumber = true;
-    if (randomNumber == 1){ 
-    	// turn LED on : TRUE STATE    
-    	digitalWrite(ledPin, HIGH);  
-    } else if(randomNumber == 2){
-        // blink LED : MAYBE STATE
-      	blinkOn = true; 
-    } else {
-      	// turn LED off : FALSE STATE    
-      	digitalWrite(ledPin, LOW);
-      	//TODO SET BUZZER HIGH
-    }
-  } 
-  
-  // we call our function to check if we blink our LED or not.
-  if (blinkOn){
-  	blink();
   }
-  
+  // check if the pushbutton is pressed.
+  if (buttonState == HIGH) {
+    Serial.print(randomNumber);
+    delay( 50 );
+    getRandomNumber = true;
+
+    if (randomNumber == 1){
+      moveServo(1);  // moveServo to TRUE state
+    } else if(randomNumber == 2){
+      moveServo(2); // moveServo to MAYBE state
+    } else {
+      moveServo(0); // movesServo to FALSE state
+    }
+  }
 }
 
-// this is a function we wrote it creates the side effect of blinking an led
-void blink(){
-  digitalWrite(ledPin, HIGH);
-  delay(1000);
-  digitalWrite(ledPin, LOW);
-  delay(1000);
+void moveServo(int fortune){
+
+  if(fortune == 1){
+    analogWrite(5, 1);
+    delay(500);
+  } else if(fortune == 2) {
+    //turn thumb to down position
+    analogWrite(5, 125);
+    delay(500);
+    } else {
+    //turn thumb sides as "maybe"
+    analogWrite(5, 255);
+    delay(500);
+  }
+}
+
+void attractMode(){
+  analogWrite(5, 1);
+  delay(500);
+
+  analogWrite(5, 125);
+  delay(500);
+
+  analogWrite(5, 255);
+  delay(500);
 }
